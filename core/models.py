dd6b5f6ec45612bc6ee38b5d095da8db03c5df80
praveen <nairxprv@gmail.com>
2024-11-14 19:29:28 +0530

    modified models

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ class todo(models.Model):
     task=models.CharField(max_length=50)
 
 class product(models.Model):
-    name=modles.CharField(max_length=50)
-    desc=modles.CharField(max_length=100)
+    name=models.CharField(max_length=50)
+    desc=models.CharField(max_length=100)
     price=models.IntegerField()
-    url = CharField(max_length=200)
+    url =models.CharField(max_length=200)
